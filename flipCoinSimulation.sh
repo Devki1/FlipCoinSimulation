3e22f728325c9b72b790fd95541d570f23a9f1f8
Devki1 <gdev3195@gmail.com>
2020-02-26 19:17:06 +0530

    ADDED:Simulator flipcoin triplet of combination

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,9 +4,19 @@ count1=0
 count2=0
 count3=0
 count4=0
+count5=0
+count6=0
+count7=0
+count8=0
+count9=0
+count10=0
+count11=0
+count12=0
+count13=0
+count14=0
 read -p "enter a value:" user
 declare -A FlipCoin
-FlipCoin=([H]=$count1 [T]=$count2 [HH]=$count3 [HT]=$count4 [TH]=$count5 [TT]=$count6)
+FlipCoin=([H]=$count1 [T]=$count2 [HH]=$count3 [HT]=$count4 [TH]=$count5 [TT]=$count6 [HHH]=$count7 [TTT]=$count8 [HHT]=$count9 [THH]=$count10 [HTT]=$count11 [TTH]=$count12 [HTH]=$count13 [THT]=$count14)
 #How many times to flip the coin by using loop
 for((iter=0;iter<$user;iter++))
 do
@@ -60,3 +70,57 @@ THPercentage=`echo "scale=2 ;(${FlipCoin[TH]}*100)/($user*2)" | bc -l`
 echo "Percentage of TH:" $THPercentage
 HTPercentage=`echo "scale=2 ; (${FlipCoin[HT]}*100)/($user*2)" | bc -l`
 echo "Percentage of HT:" $HTPercentage
+#Simulator the triplet combination
+for((iter=0;iter<$user*3;iter++))
+do
+   checkFlipCoin=$((RANDOM%6))
+
+   case $checkFlipCoin in  				
+		0)		
+			FlipCoin[HHH]=$((${FlipCoin[HHH]}+1))
+			;;
+		1)
+			FlipCoin[TTT]=$((${FlipCoin[TTT]}+1))
+			;;
+		2)
+			FlipCoin[HHT]=$((${FlipCoin[HHT]}+1))
+			;;
+		3)
+			FlipCoin[TTH]=$((${FlipCoin[TTH]}+1))
+			;;
+		4)
+			FlipCoin[THT]=$((${FlipCoin[THT]}+1))
+			;;
+		5)
+			FlipCoin[HTH]=$((${FlipCoin[HTH]}+1))
+			;;
+		6)
+			FlipCoin[HTT]=$((${FlipCoin[HTT]}+1))
+			;;
+		7)
+			FlipCoin[THH]=$((${FlipCoin[THH]}+1))
+			;;
+	esac
+done
+for key in "${!FlipCoin[@]}"
+do
+   echo "${key}:${FlipCoin[$key]}"
+done 
+HHHPercentage=`echo "scale=4 ; (${FlipCoin[HHH]}*100)/($user*3)" | bc -l`
+echo "Percentage of HHH :" $HHHPercentage
+TTTPercentage=`echo "scale=4 ; (${FlipCoin[TTT]}*100)/($user*3)" | bc -l`
+echo "Percentage of TTT:" $TTTPercentage
+TTHPercentage=`echo "scale=4 ;(${FlipCoin[TTH]}*100)/($user*3)" | bc -l`
+echo "Percentage of TTH:" $TTHPercentage
+THTPercentage=`echo "scale=4 ; (${FlipCoin[THT]}*100)/($user*3)" | bc -l`
+echo "Percentage of HT:" $THTPercentage
+HTTPercentage=`echo "scale=4 ; (${FlipCoin[HTT]}*100)/($user*3)" | bc -l`
+echo "Percentage of HTT :" $HTTPercentage
+THHPercentage=`echo "scale=4 ; (${FlipCoin[THH]}*100)/($user*3)" | bc -l`
+echo "Percentage of TTT:" $THHPercentage
+HHTPercentage=`echo "scale=4 ;(${FlipCoin[HHT]}*100)/($user*3)" | bc -l`
+echo "Percentage of HHT:" $HHTPercentage
+HTHPercentage=`echo "scale=4 ; (${FlipCoin[HTH]}*100)/($user*3)" | bc -l`
+echo "Percentage of HTH:" $HTHPercentage
+
+
