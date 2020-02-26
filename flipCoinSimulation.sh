6d28fc24e5a821b61d38d5d0a92c67d8ab5f565b
Devki1 <gdev3195@gmail.com>
2020-02-26 16:05:45 +0530

    ADDED:Singlet combination of head and tail and percentage of head and tail

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -x
-isHead=1
-isTail=0
-#Generate a two random number
-checkflipCoin=$((RANDOM%2))
-if((checkflipCoin == isHead))
-then
-	echo "coin isHead"
-else
-	echo "coin isTail"
-fi 
-
+#declare constant
+count_H=0
+count_T=0
+read -p "enter a value:" user
+declare -A FlipCoin	
+#How many times to flip the coin by using loop
+for((iter=0;iter<$user;iter++))
+do
+	checkFlipCoin=$((RANDOM%2))
+	case $checkFlipCoin in
+		0)
+			FlipCoin[$iter]="H"
+			((Count_H++))
+			;;
+		1)
+			FlipCoin[$iter]="T"
+			((Count_T++))
+			;;
+	esac
+done
+echo "generate singlet head and tail" ${FlipCoin[@]} 
+PercentageofHead=`echo "scale=2 ; ($Count_H*100)/$user" | bc -l`
+echo "Percentage of Head :" $PercentageofHead
+PercentageofTail=`echo "scale=2 ; ($Count_T*100)/$user" | bc -l`
+echo "Percentage of Tail :" $PercentageofTail 
